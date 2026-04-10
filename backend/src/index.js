42a30c4fea8e2c23328b8a5c78037165cb04c2c6
Manjeet Kumar <mkj22834@gmail.com>
2026-04-10 19:09:01 +0530

    env modified

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:process.env.CORS_ORIGIN,
   credentials:true
 }))
 // userAuth apis
